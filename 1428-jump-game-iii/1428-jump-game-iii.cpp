60116a75d84e8ec2fe7f9da0e477d8b1e030b575
Pragati Mishra <87942959+Pragatimishra21@users.noreply.github.com>
2023-06-14 12:32:24 +0530

    Time: 50 ms (61.62%), Space: 32.2 MB (66.19%) - LeetHub

---
+++ 1428-jump-game-iii/1428-jump-game-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+    int n = arr.size();
+    queue<int> q;
+    q.push(start);
+    vector<int> vis(n);
+    vis[start] = 1;
+
+    while(q.size())
+    {
+        int i = q.front();
+        q.pop();
+        if(arr[i] == 0) return true;
+
+        if(i+arr[i] <n && !vis[i+arr[i]])
+        {
+          q.push(i+arr[i]);
+          vis[i+arr[i]] = 1;
+        }
+
+        if(i-arr[i]>=0 && !vis[i-arr[i]])
+        {
+            q.push(i-arr[i]);
+            vis[i-arr[i]] = 1;
+        }
+    }
+    return false;
+    }
+};
